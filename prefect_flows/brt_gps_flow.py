0318cc3ff304100a1e185e40c6648c8d3e6f5f51
AryaneD <aryanecristine2@gmail.com>
2025-02-23 20:55:11 -0300

    Update brt_gps_flow

--- prefect_flows/brt_gps_flow.py
+++ prefect_flows/brt_gps_flow.py
@@ -3,28 +3,45 @@ import requests
 import pandas as pd
 from datetime import datetime
 from sqlalchemy import create_engine
+import os
 
 # Defina a tarefa para capturar os dados
 @task
 def fetch_data():
     url = "https://dados.mobilidade.rio/gps/brt"
-    response = requests.get(url)
-    return response.json()
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Levanta erro para status HTTP não OK
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        raise ValueError(f"Erro ao obter dados da API: {e}")
 
 # Defina a tarefa para processar os dados e salvar em CSV
 @task
 def process_data(data):
     df = pd.json_normalize(data)
     filename = f"brt_data_{datetime.now().strftime('%Y%m%d%H%M%S')}.csv"
-    df.to_csv(filename, index=False)
-    return filename
+    try:
+        # Salvando o arquivo CSV com a codificação UTF-8 para garantir que caracteres especiais sejam corretamente tratados
+        df.to_csv(filename, index=False, encoding='utf-8')
+        return filename
+    except Exception as e:
+        raise ValueError(f"Erro ao salvar o arquivo CSV: {e}")
 
 # Defina a tarefa para carregar os dados no PostgreSQL
 @task
 def load_to_postgresql(csv_file):
-    df = pd.read_csv(csv_file)
-    engine = create_engine('postgresql://user:password@127.0.0.1:5433:5433/brt_db')
-    df.to_sql('brt_gps_data', con=engine, if_exists='append', index=False)
+    try:
+        # Lendo o arquivo CSV com a codificação UTF-8 para garantir a correta leitura dos caracteres especiais
+        df = pd.read_csv(csv_file, encoding='utf-8')
+        engine = create_engine('postgresql://user:password@127.0.0.1:5433/brt_db')
+        # Especificando os tipos de dados de acordo com o DataFrame
+        df.to_sql('brt_gps_data', con=engine, if_exists='append', index=False)
+    except Exception as e:
+        raise ValueError(f"Erro ao carregar os dados no PostgreSQL: {e}")
+    finally:
+        # Fechar a conexão com o banco de dados, se necessário
+        engine.dispose()
 
 # Crie o flow
 with Flow("brt_gps_flow") as flow:
