45914b7ea4f4efdd61c525e3b93fd6501dd88a3c
AryaneD <aryanecristine2@gmail.com>
2025-02-24 05:11:39 -0300

    fix: change json extract

--- dbt_models/transform_brt_gps.sql
+++ dbt_models/transform_brt_gps.sql
@@ -2,16 +2,16 @@ WITH veiculos_extracao AS (
     SELECT
         id,
         -- Desempacotando o JSON
-        JSON_EXTRACT(veiculos, '$[0].latitude') AS latitude,
-        JSON_EXTRACT(veiculos, '$[0].longitude') AS longitude,
-        JSON_EXTRACT(veiculos, '$[0].velocidade') AS velocidade
+        (veiculos->0->>'latitude')::FLOAT AS latitude,
+        (veiculos->0->>'longitude')::FLOAT AS longitude,
+        (veiculos->0->>'velocidade')::FLOAT AS velocidade
     FROM {{ ref('brt_gps_data') }}
 )
 SELECT
     id,
-    CAST(latitude AS FLOAT) AS latitude,
-    CAST(longitude AS FLOAT) AS longitude,
-    CAST(velocidade AS FLOAT) AS velocidade
+    latitude,
+    longitude,
+    velocidade
 FROM veiculos_extracao
 WHERE latitude IS NOT NULL
   AND longitude IS NOT NULL
