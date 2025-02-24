1d090b6ed7c4f019525fdb60c44262600ebb5692
AryaneD <aryanecristine2@gmail.com>
2025-02-24 05:02:37 -0300

    feat: dbt execution models

--- dbt_models/transform_brt_gps.sql
+++ dbt_models/transform_brt_gps.sql
@@ -1,8 +1,18 @@
-with brt_data as (
-    select 
+WITH veiculos_extracao AS (
+    SELECT
         id,
-        position,
-        speed
-    from {{ ref('brt_gps_data') }}
+        -- Desempacotando o JSON
+        JSON_EXTRACT(veiculos, '$[0].latitude') AS latitude,
+        JSON_EXTRACT(veiculos, '$[0].longitude') AS longitude,
+        JSON_EXTRACT(veiculos, '$[0].velocidade') AS velocidade
+    FROM {{ ref('brt_gps_data') }}
 )
-select * from brt_data
+SELECT
+    id,
+    CAST(latitude AS FLOAT) AS latitude,
+    CAST(longitude AS FLOAT) AS longitude,
+    CAST(velocidade AS FLOAT) AS velocidade
+FROM veiculos_extracao
+WHERE latitude IS NOT NULL
+  AND longitude IS NOT NULL
+  AND velocidade IS NOT NULL;
