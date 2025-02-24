e45f133213010ed77397ff361c938122e7f09a46
AryaneD <aryanecristine2@gmail.com>
2025-02-24 01:31:35 -0300

    feat: add client_encoding=utf8

--- prefect_flows/brt_gps_flow.py
+++ prefect_flows/brt_gps_flow.py
@@ -39,7 +39,7 @@ def load_to_postgresql(csv_file):
         if not os.path.exists(csv_file):
             raise FileNotFoundError(f"O arquivo {csv_file} não foi encontrado.")
         df = pd.read_csv(csv_file)
-        engine = create_engine('postgresql://user:password@localhost:5433/postgres')
+        engine = create_engine('postgresql://user:password@localhost:5433/postgres?client_encoding=utf8')
         df.to_sql('brt_gps_data', con=engine, if_exists='append', index=False)
     except Exception as e:
         raise ValueError(f"Erro ao carregar dados para o PostgreSQL: {e}")
